31528e6d14daa98ca13a63374acee4776e6dd809
Emile Shodiev <kaizzzer@gmail.com>
2021-07-01 00:39:56 +0300

    no user mention by default

--- src/util/user-signature.ts
+++ src/util/user-signature.ts
@@ -1,6 +1,6 @@
 import { User } from "../domain/User";
 
-export const userSignature = (user: User, withSobaka = true) => {
+export const userSignature = (user: User, withSobaka = false) => {
   if (user.username) {
     return (withSobaka ? "@" : "") + user.username;
   } else {
